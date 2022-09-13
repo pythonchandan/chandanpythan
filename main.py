2573193a20a032bda5d2ca4709d2458a8f60c4a6
pythonchandan <113375248+pythonchandan@users.noreply.github.com>
2022-09-13 09:37:09 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,40 +1,15 @@
-print("Welcome to my computer quiz!")
-
-playing = input("Do you want to play? ")
-
-if playing.lower() != "yes":
-    quit()
-
-print("Okay! Let's play :)")
-score = 0
-
-answer = input("What does CPU stand for? ")
-if answer.lower() == "central processing unit":
-    print('Correct!')
-    score += 1
-else:
-    print("Incorrect!")
-
-answer = input("What does GPU stand for? ")
-if answer.lower() == "graphics processing unit":
-    print('Correct!')
-    score += 1
-else:
-    print("Incorrect!")
-
-answer = input("What does RAM stand for? ")
-if answer.lower() == "random access memory":
-    print('Correct!')
-    score += 1
-else:
-    print("Incorrect!")
-
-answer = input("What does PSU stand for? ")
-if answer.lower() == "power supply":
-    print('Correct!')
-    score += 1
-else:
-    print("Incorrect!")
-
-print("You got " + str(score) + " questions correct!")
-print("You got " + str((score / 4) * 100) + "%.")
+name=input("enter your name")
+print("hello",name+"!")
+birthyear=int(input("please enter your of birth"))
+age=2021-birthyear
+print("your age is",age)
+a=8
+print("id of a=",id(a))
+b=a
+print("id of b=",id(b))
+c=6
+print("id of c=",id(c))
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+sum=a+b
+print("sum",sum)
